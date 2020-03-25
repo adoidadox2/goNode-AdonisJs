51ecd81dee716d204525feb47c11f0c72f116bf8
adoidadox2 <augusto.geek@gmail.com>
2020-03-25 03:09:06 -0300

    Finished ( Missing Sentry and CORS )

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Mail = use('Mail');
+const Helpers = use('Helpers');
+
+class NewTaskMail {
+  static get concurrency() {
+    return 1;
+  }
+
+  static get key() {
+    return 'NewTaskMail-job';
+  }
+
+  async handle({ email, username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`);
+    await Mail.send(
+      ['emails.new_task'],
+      {
+        username,
+        title,
+        hasAttachment: !!file,
+      },
+      message => {
+        message
+          .to(email)
+          .from(
+            'armandinho@fulodemandacaru.com',
+            'Armandinho | Puxador de fole'
+          )
+          .subject('Nova Task para você');
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name,
+          });
+        }
+      }
+    );
+  }
+}
+
+module.exports = NewTaskMail;
